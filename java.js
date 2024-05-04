1d83dbf10650437cfd442f336c05b344fa47f2fe
Nboy1027 <noam1027@gmail.com>
2024-05-04 12:40:07 +0300

    Add scoring and evaluation of choice algorithm

--- java.js
+++ java.js
@@ -1,3 +1,13 @@
+
+let humanScore=0;
+
+let computerScore=0;
+
+
+
+
+
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max)
 }
@@ -34,14 +44,66 @@ function ComputerChoice() {
 }
 
 
-function humanChoice() {
-
-let Hchoice = prompt("Choose 'Rock', 'Paper' or 'Scissors'")
+function humanChoice(Hchoice) {
 
 Hchoice=Hchoice.charAt(0).toUpperCase() + Hchoice.slice(1).toLowerCase();
-
 return Hchoice;
 }
 
 
-console.log(humanChoice());
+
+function playRound(humanChoice, ComputerChoice){
+
+
+if (humanChoice===ComputerChoice) {
+
+console.log("DRAW");
+
+}
+
+else if (humanChoice==="Rock" && ComputerChoice==="Scissors") {
+    console.log("You win!")
+
+    humanScore++;
+}
+else if (humanChoice==="Paper" && ComputerChoice==="Rock"){
+
+    console.log("You win!")
+
+    humanScore++;
+
+
+}
+
+else if (humanChoice==="Scissors" && ComputerChoice==="Paper"){
+
+    console.log("You win!")
+
+    humanScore++;
+
+
+}
+
+else {
+    console.log("You lost!")
+
+    computerScore ++;
+}
+}
+
+
+const humanSelection= humanChoice(prompt("Choose 'Rock', 'Paper' or 'Scissors'"));
+const computerSelection = ComputerChoice();
+
+console.log("You Chose: "+ humanSelection);
+console.log("Your Oponent Chose: " +computerSelection);
+
+
+
+playRound(humanSelection, computerSelection);
+
+console.log("Human Score: " + humanScore + " | Oponenet Score: " + computerScore);
+
+
+
+
